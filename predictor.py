69306028ccfaa8bcb8df53d9c9076ca5b59e3754
Tyler Hilbert <tylerjoshuahilbert@gmail.com>
2016-12-15 01:30:10 -0500

    Added prediction based off of pclass instead of just sex.... this made my model go down.. I should make a script to test locally before submitting in the future....... Also added jupyter notebook

--- predictor.py
+++ predictor.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import numpy as np
 
-def didSurvive(sex):
-    return (sex == "female").astype(int)
+def didSurvive(sex, pclass):
+    return np.logical_and(sex == "female", pclass == 1).astype(int)
 
 
 titanic_df = pd.read_csv("test.csv")
+
 submission = pd.DataFrame({
     "PassengerId": titanic_df["PassengerId"],
-    "Survived": didSurvive(titanic_df["Sex"])
+    "Survived": didSurvive(titanic_df["Sex"], titanic_df["Pclass"])
 })
 submission.to_csv('titanic-predictions.csv', index=False)
--- survival_count.py
+++ survival_count.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 titanic_df = pd.read_csv("train.csv")
 print(titanic_df['Survived'].value_counts())
+
+#TODO - how can I divide these values by eachother to get the ratio?
+print(titanic_df['Pclass'].value_counts())
+print(titanic_df.loc[titanic_df['Survived'] == 1]['Pclass'].value_counts())
