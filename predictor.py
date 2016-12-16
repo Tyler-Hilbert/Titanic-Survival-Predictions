e210a456c317544f4b86df9512a6da347c84434e
Tyler Hilbert <tylerjoshuahilbert@gmail.com>
2016-12-16 01:51:09 -0500

    Improved score by adding age < 10 && embarced c

--- predictor.py
+++ predictor.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def didSurvive(sex, age):
-    return np.logical_or(sex == "female", age <= 15).astype(int)
+def didSurvive(sex, embarked, age):
+    return np.logical_or(sex == "female", np.logical_and(age <= 10, embarked == 'C')).astype(int)
 
 
 titanic_df = pd.read_csv("test.csv")
 
 submission = pd.DataFrame({
     "PassengerId": titanic_df["PassengerId"],
-    "Survived": didSurvive(titanic_df["Sex"], titanic_df["Age"])
+    "Survived": didSurvive(titanic_df["Sex"], titanic_df["Embarked"], titanic_df["Age"])
 })
 submission.to_csv('titanic-predictions.csv', index=False)
