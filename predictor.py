174adfef8235cf706078e6945483080f9b72616d
Tyler Hilbert <tylerjoshuahilbert@gmail.com>
2016-12-15 22:51:10 -0500

    Added plotting/counting each field and the survival rates.  Added age into prediction

--- predictor.py
+++ predictor.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def didSurvive(sex, pclass):
-    return np.logical_and(sex == "female", pclass == 1).astype(int)
+def didSurvive(sex, age):
+    return np.logical_or(sex == "female", age >= 15).astype(int)
 
 
 titanic_df = pd.read_csv("test.csv")
 
 submission = pd.DataFrame({
     "PassengerId": titanic_df["PassengerId"],
-    "Survived": didSurvive(titanic_df["Sex"], titanic_df["Pclass"])
+    "Survived": didSurvive(titanic_df["Sex"], titanic_df["Age"])
 })
 submission.to_csv('titanic-predictions.csv', index=False)
--- survival_count.py
+++ survival_count.py
@@ -1,8 +1,28 @@
 import pandas as pd
 
-titanic_df = pd.read_csv("train.csv")
-print(titanic_df['Survived'].value_counts())
+# Print how many people lived with a field
+def printFieldSurvival(df, field):
+    print(field)
+    #TODO - how can I divide these values by eachother to get the ratio?
+    print("Number of people:")
+    print(df[field].value_counts())
+    print("Number of people survived:")
+    print(df.loc[df['Survived'] == 1][field].value_counts())
+    print()
 
-#TODO - how can I divide these values by eachother to get the ratio?
-print(titanic_df['Pclass'].value_counts())
-print(titanic_df.loc[titanic_df['Survived'] == 1]['Pclass'].value_counts())
+titanic_df = pd.read_csv("train.csv")
+
+# Print amount survived
+print("Amount survived")
+print(titanic_df['Survived'].value_counts())
+print();
+
+# Print fields
+printFieldSurvival(titanic_df, 'Pclass')
+printFieldSurvival(titanic_df, 'SibSp')
+printFieldSurvival(titanic_df, 'Embarked')
+printFieldSurvival(titanic_df, 'Parch')
+
+
+# Create graph
+print (titanic_df['Age'].hist(bins=15))
